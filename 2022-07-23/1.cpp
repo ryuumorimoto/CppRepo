b35ceb247e051a4382e2fc1367f30dc92f31f1f0
ryuumorimoto <morimotoryu@bmcomp.jp>
2022-09-21 20:00:27 +0900

    9/21

--- 2022-07-23/1.cpp
+++ 2022-07-23/1.cpp
@@ -2,25 +2,39 @@
 
 using namespace std;
 
-int main(){
-    int L1,R1,L2,R2;
+int main()
+{
+    int L1, R1, L2, R2;
     cin >> L1 >> R1 >> L2 >> R2;
     int ans;
-    if(L1 <= L2 && L2 < R1){
-        if(R2 > R1){
-                ans = R1 - L2;    
-        }else{
+    if (L1 <= L2 && L2 < R1)
+    {
+        if (R2 > R1)
+        {
+            ans = R1 - L2;
+        }
+        else
+        {
             ans = R2 - L2;
         }
-    }else if(L1 < R2 && R2 <= R1){
-        if(L2 < L1){
+    }
+    else if (L1 < R2 && R2 <= R1)
+    {
+        if (L2 < L1)
+        {
             ans = R2 - L1;
-        }else{
+        }
+        else
+        {
             ans = R2 - L2;
         }
-    }else if(L2 <= L1 && R1 <= R2){
+    }
+    else if (L2 <= L1 && R1 <= R2)
+    {
         ans = R1 - L1;
-    }else{
+    }
+    else
+    {
         ans = 0;
     }
 
---
+++ dataAlgorithm/3-5.cpp
@@ -0,0 +1,6 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() { return 0; }
